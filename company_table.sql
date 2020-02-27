161b8153e1ad7e16a72620b0a26065e76249e20c
kameshkrishna <61307414+kameshkrishna@users.noreply.github.com>
2020-02-27 14:39:24 +0530

    company_table

---
+++ company_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE COMPANY(
+   id serial PRIMARY KEY,
+   name VARCHAR (255) NOT NULL,
+   url VARCHAR (255) UNIQUE NOT NULL,
+   address1 VARCHAR (255) NULL,
+   address2 VARCHAR (255) NULL,
+   city VARCHAR (255) NULL,
+   state VARCHAR (255) NULL,
+   country VARCHAR (255) NULL,
+   phone VARCHAR (255) NULL,
+   created_on TIMESTAMP NOT NULL,
+   modified_on TIMESTAMP NOT NULL
+);
